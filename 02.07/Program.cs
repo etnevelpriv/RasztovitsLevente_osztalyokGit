6dcc7eaa3081da490c402d974d36b7d7d32bb5d2
Levente Rasztovits <rasztovits.levente@petrik.hu>
2025-02-07 10:44:24 +0100

    Add project files.

---
+++ 02.07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Konyv konyv1 = new Konyv("Az ember tragédiája", "Madách Imre", 2021, 350, 5000);
+            int teljesOsszeg = konyv1.Arnoveles(500);
+            konyv1.Kedvezmeny(40);
+            int maradekOldal = konyv1.HanyOldalMaradt(100);
+            Console.WriteLine(konyv1);
+            Console.WriteLine($"{konyv1.Cim} teljes összege adóval: {teljesOsszeg}, amíg a kedvezményes árral: {konyv1.Ar}. Már egy kicsit olvastam belőle, a maradék oldalszám:aradék oldalszám: {maradekOldal}");
+            Console.WriteLine();
+
+
+            Konyv konyv2 = new Konyv("Egri csillagok", "Gárdonyi Géza");
+            Console.WriteLine(konyv2);
+        }
+    }
+}
---
+++ 02.07/konyv.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace _02._07
+{
+    internal class Konyv
+    {
+        private string cim;
+        private string szerzo;
+        private int kiadasEve;
+        private int oldalszam;
+        private int ar;
+
+        public string Cim
+        {
+            get { return cim; }
+            set { cim = value; }
+        }
+
+        public string Szerzo
+        {
+            get { return szerzo; }
+            set { szerzo = value; }
+        }
+
+        public int KiadasEve
+        {
+            get { return kiadasEve; }
+            set { kiadasEve = value; }
+        }
+
+        public int Oldalszam
+        {
+            get { return oldalszam; }
+            set { oldalszam = value; }
+        }
+
+        public int Ar
+        {
+            get { return ar; }
+            set { ar = value; }
+        }
+
+        public Konyv(string cim, string szerzo, int kiadasEve, int oldalszam, int ar)
+        {
+            this.cim = cim;
+            this.szerzo = szerzo;
+            this.kiadasEve = kiadasEve;
+            this.oldalszam = oldalszam;
+            this.ar = ar;
+        }
+
+        public Konyv(string cim, string szerzo)
+        {
+            this.cim = cim;
+            this.szerzo = szerzo;
+            this.kiadasEve = DateTime.Now.Year;
+            this.oldalszam = 200;
+            this.ar = 3000;
+        }
+
+        public int Arnoveles(int osszeg)
+        {
+            return ar += osszeg;
+        }
+
+        public int Kedvezmeny(double szazalek)
+        {
+            return ar -= (int)(ar * (szazalek / 100));
+        }
+
+        public int HanyOldalMaradt(int elolvasott)
+        {
+            return oldalszam - elolvasott;
+        }
+
+        public override string ToString()
+        {
+            return $"Cím: {cim}, Szerző: {szerzo}, Kiadás éve: {kiadasEve}, Oldalszám: {oldalszam}, Ár: {ar} Ft";
+        }
+    }
+}
