18fbed3c3f68fcdaad05208c5bb4d21a5b60a79c
Levente Rasztovits <rasztovits.levente@petrik.hu>
2025-02-13 19:16:48 +0100

    Második feladat elkészült. Film osztály létrehozva.

---
+++ 02.07/Film.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._07
+{
+    internal class Film
+    {
+        public string Cim { get; set; }
+        public string Rendezo { get; set; }
+        public int HosszPercekben { get; set; }
+        public string Mufaj { get; set; }
+        public bool Megjelent { get; set; }
+
+        public Film(string cim, string rendezo, int hosszPercekben, string mufaj, bool megjelent)
+        {
+            Cim = cim;
+            Rendezo = rendezo;
+            HosszPercekben = hosszPercekben;
+            Mufaj = mufaj;
+            Megjelent = megjelent;
+        }
+
+        public Film(string cim, string rendezo)
+        {
+            Cim = cim;
+            Rendezo = rendezo;
+            HosszPercekben = 120;
+            Mufaj = "Ismeretlen";
+            Megjelent = false;
+        }
+
+        public void Jatszas()
+        {
+            Console.WriteLine("A film elkezdődött.");
+        }
+
+        public void MufajModositas(string ujMufaj)
+        {
+            Mufaj = ujMufaj;
+        }
+
+        public void HosszNovelese(int percek)
+        {
+            HosszPercekben += percek;
+        }
+
+        public override string ToString()
+        {
+            return $"Cím: {Cim}, Rendező: {Rendezo}, Hossz: {HosszPercekben} perc, Műfaj: {Mufaj}, Megjelent: {Megjelent}";
+        }
+    }
+}
--- 02.07/Program.cs
+++ 02.07/Program.cs
@@ -8,8 +8,11 @@ namespace _02._07
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Konyv(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Könyv Class:");
+            Console.ForegroundColor = ConsoleColor.White;
             Konyv konyv1 = new Konyv("Az ember tragédiája", "Madách Imre", 2021, 350, 5000);
             int teljesOsszeg = konyv1.Arnoveles(500);
             konyv1.Kedvezmeny(40);
@@ -18,10 +21,29 @@ namespace _02._07
             Console.WriteLine($"{konyv1.Cim} teljes összege adóval: {teljesOsszeg}, amíg a kedvezményes árral: {konyv1.Ar}. Már egy kicsit olvastam belőle, a maradék oldalszám:aradék oldalszám: {maradekOldal}");
             Console.WriteLine();
 
-
-
             Konyv konyv2 = new Konyv("Egri csillagok", "Gárdonyi Géza");
             Console.WriteLine(konyv2);
+            Console.WriteLine();
+        }
+
+        static void Film(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Film Class:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Film film1 = new Film("A keresztapa", "Francis Ford Coppola", 175, "dráma", true);
+            film1.Jatszas();
+            film1.MufajModositas("bűnügyi");
+            film1.HosszNovelese(10);
+            Console.WriteLine(film1);
+            Console.WriteLine();
+            Film film2 = new Film("Titanic", "James Cameron");
+            Console.WriteLine(film2);
+        }
+        static void Main(string[] args)
+        {
+            Konyv(args);
+            Film(args);
         }
     }
 }
